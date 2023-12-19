24d086889f0d40dca35c815dbf11aba72cc731da
thebrownkidd <139674706+thebrownkidd@users.noreply.github.com>
2023-12-19 20:35:51 +0530

    coding notes added and information added to localloc.txt

--- Guide.py
+++ Guide.py
@@ -1,6 +1,7 @@
 import string as st
 import pyperclip as ppc
-
+import subprocess
+import webbrowser
 boilp = {
     "c": '#import<stdio.h>\n#import<conio.h>\nint main(){\n\t\n\nreturn 0;}',
     "C": '#import<stdio.h>\n#import<conio.h>\nint main(){\n\t\n\nreturn 0;}',
@@ -12,16 +13,27 @@ boilp = {
     "HTML": '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="UTF-8">\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t\t<meta http-equiv="X-UA-Compatible" content="ie=edge">\n\t\t<title>HTML 5 Boilerplate</title>\n\t\t<link rel="stylesheet" href="style.css">\n\t</head>\n\t<body>\n\t\t<script src="index.js"></script>\n\t</body>\n</html>'
     
 }
+notes = {
+    "regression" : ["web","insert link"]
+}
 
 def codehelp(topic):
     match topic:
         case "boilerplate":
             lang = input("Enter language: ")
-            lang.lower()
             print("The boilerplate for", lang, " is copied to your clipboard")
             boil = str(boilp[lang])
             print(boil)
             ppc.copy(boil)
+        case "Technology":
+            tech = input("Which technology are you working on")
+            print("Notes for ",tech," are on your screen now")
+            tech = tech.lower()
+            ret = str(notes[tech])
+            if ret[0] == "local":
+                subprocess.call(ret[1])
+            elif ret[0] == "web":
+                webbrowser.open(ret[1])
 def direct(a):
     if a[1] == "code":
         codehelp(a[2])
