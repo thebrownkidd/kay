fcb71fa68effa1ba06d473f6a6c1b697d16ee92f
thebrownkidd <139674706+thebrownkidd@users.noreply.github.com>
2023-12-19 15:06:24 +0530

    json file to store location implemented

--- Automation.py
+++ Automation.py
@@ -8,9 +8,21 @@ filoc = {}
 def webop(site):
     webbrowser.open("http://www."+site+".com")
     
+def filsearch(file):
+    global loc
+    global fcount
+    fcount = 0 
+    obj = open('localloc.txt','r')
+    dic = json.loads(obj.read())
+    for x in dic.keys():
+        if x == file:
+            loc = dic[file]
+            fcount = 1
+            break
+
 def searchop(fil,folder):
-        fsearch(fil,folder)
-        f3 = locat.replace("\\","/")
+        f3 = combsearch(fil,folder)
+        f3 = f3.replace("\\","/")
         subprocess.call(f3)
 def fsearch(file, folder):
     global f2
@@ -31,6 +43,21 @@ def fsearch(file, folder):
                     fsearch(file,folder+f+"\\")
         except:
             pass
+
+def combsearch (file, folder):
+    filsearch(file)
+    if fcount != 0:
+        return loc
+    else:
+        fsearch(file, folder)
+        obj = open('localloc.txt','r')
+        temp = json.loads(obj.read())
+        temp[file] = locat
+        obj.close()
+        obj2 = open('localloc.txt','w')
+        obj2.write(json.dumps(temp))
+        obj2.close
+        return locat
 def auto(a):
     if a[1] == "run":
         searchop(a[2]+".exe","C:\\")
